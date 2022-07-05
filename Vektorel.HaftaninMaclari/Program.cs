0c223e9dfe2697e799bc35ef382aa2510fd589f7
İlker Ünver <108666062+IlkerUnver00@users.noreply.github.com>
2022-07-05 13:19:02 +0300

    Add files via upload

---
+++ Vektorel.HaftaninMaclari/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vektorel.HaftaninMaclari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string mac = "Fenerbahçe : 0 - 0 : Galatasaray ";
+
+            string[] takimlar =
+            {
+                "Beşiktaş","Fenerbahçe","Galatasaray","Trabzonspor"
+            };
+
+
+            string[] maclar = new string[9];
+            Console.WriteLine("Haftanın maçları uygulmasına hoş geldiniz");
+            int eklenenTakimSayisi = 0;
+            Random rnd = new Random();
+            while (true)
+            {
+                Console.WriteLine("1-Maç Oluştur\n2-Maçları görüntüle");
+                char c = Console.ReadKey(true).KeyChar;
+                if (c=='1')
+                {
+                    Console.Write("Ev takımı:");
+                    string takima = Console.ReadLine();
+                    Console.Write("Deplasman takımı:");
+                    string takimb = Console.ReadLine();
+
+                    maclar[eklenenTakimSayisi] = $"{takima} : {rnd.Next(6)} - {rnd.Next(5)} : {takimb}";
+                    eklenenTakimSayisi++;
+                }
+                else if (c == '2')
+                {
+                    for (int i = 0; i < eklenenTakimSayisi; i++)
+                    {
+                        Console.WriteLine(maclar[i]);
+                    }
+                }
+
+            }
+
+        }
+    }
+}
